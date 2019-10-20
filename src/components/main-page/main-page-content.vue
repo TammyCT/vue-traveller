745281eaae1e3744f0ff21050c38c46da7a86eec
TammyCT <471636895@qq.com>
2019-10-20 18:05:25 +0800

    update search bar position

---
+++ src/components/main-page/main-page-content.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="main-box">
+      <el-row :gutter="8" class="group-outer-box">
+      <el-col :span="2" v-for="item of searchGroup">
+        <el-card shadow="hover" class="group-box">
+          {{item}}
+        </el-card>
+      </el-col>
+    </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "main-page-content",
+      data(){
+          return{
+            searchGroup : ['自由行','跟团游','机票','酒店','签证','邮轮']
+          }
+      }
+    }
+</script>
+
+<style scoped>
+  .group-box{
+    cursor: pointer;
+  }
+  .group-outer-box{
+    display: flex;
+    width: 100%;
+    flex-direction: row;  /* 子元素横向排列 */
+    justify-content: center; /* 相对父元素水平居中 */
+  }
+
+  .main-box{
+    padding: 10px;
+  }
+
+</style>
